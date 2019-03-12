d279a327bad3e42766f3990fc68206500ad3653a
QienJiang <a804242681lrm@gmail.com>
2019-03-12 15:04:29 -0400

    json
    xxx

--- map-app-react/src/App.js
+++ map-app-react/src/App.js
@@ -12,30 +12,29 @@ import home from './home';
 class App extends Component {
   render() {
     return (
-      <Router basename="/react-auth-ui/">
+      <Router basename="/homepage/">
         <div className="App">
         <div className="Map">
         <Map/>
         </div>
           <div className="App__Form" >
-              <Route exact path="/" component={home}>
-              </Route>
-            {/*<div className="PageSwitcher">*/}
-                {/*<NavLink to="/sign-in" activeClassName="PageSwitcher__Item--Active" className="PageSwitcher__Item">Sign In</NavLink>*/}
-                {/*<NavLink exact to="/" activeClassName="PageSwitcher__Item--Active" className="PageSwitcher__Item">Sign Up</NavLink>*/}
-                {/*<NavLink exact to="/Guest" activeClassName="PageSwitcher__Item--Active" className="PageSwitcher__Item">Guest</NavLink>*/}
-              {/*</div>*/}
 
-              {/*<div className="FormTitle">*/}
-                  {/*<NavLink to="/sign-in" activeClassName="FormTitle__Link--Active" className="FormTitle__Link">Sign In</NavLink> or <NavLink exact to="/" activeClassName="FormTitle__Link--Active" className="FormTitle__Link">Sign Up</NavLink>*/}
-                  {/*or <NavLink exact to="/Guest" activeClassName="FormTitle__Link--Active" className="FormTitle__Link">Guest</NavLink>*/}
-              {/*</div>*/}
-              {/*<Route exact path="/" component={SignUp}>*/}
-              {/*</Route>*/}
-              {/*<Route path="/sign-in" component={SignIn}>*/}
-              {/*</Route>*/}
-              {/*<Route exact path="/Guest" component={Guest}>*/}
-              {/*</Route>*/}
+            <div className="PageSwitcher">
+                <NavLink to="/sign-in" activeClassName="PageSwitcher__Item--Active" className="PageSwitcher__Item">Sign In</NavLink>
+                <NavLink exact to="/sign-up" activeClassName="PageSwitcher__Item--Active" className="PageSwitcher__Item">Sign Up</NavLink>
+                <NavLink exact to="/Guest" activeClassName="PageSwitcher__Item--Active" className="PageSwitcher__Item">Guest</NavLink>
+              </div>
+
+              <div className="FormTitle">
+                  <NavLink to="/sign-in" activeClassName="FormTitle__Link--Active" className="FormTitle__Link">Sign In</NavLink> or <NavLink exact to="/sign-up" activeClassName="FormTitle__Link--Active" className="FormTitle__Link">Sign Up</NavLink>
+                  or <NavLink exact to="/Guest" activeClassName="FormTitle__Link--Active" className="FormTitle__Link">Guest</NavLink>
+              </div>
+              <Route exact path="/sign-up" component={SignUp}>
+              </Route>
+              <Route path="/sign-in" component={SignIn}>
+              </Route>
+              <Route exact path="/Guest" component={Guest}>
+              </Route>
           </div>
 
         </div>
--- map-app-react/src/loginpage/SignIn.js
+++ map-app-react/src/loginpage/SignIn.js
@@ -1,60 +1,78 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import axios from 'axios';
 
 class SignInForm extends Component {
-    constructor() {
-        super();
+  constructor() {
+    super();
 
-        this.state = {
-            email: '',
-            password: ''
-        };
+    this.state = {
+      email: '',
+      password: '',
+      login:false,
+    };
 
-        this.handleChange = this.handleChange.bind(this);
-        this.handleSubmit = this.handleSubmit.bind(this);
-    }
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+  componentDidMount(){
 
-    handleChange(e) {
-        let target = e.target;
-        let value = target.type === 'checkbox' ? target.checked : target.value;
-        let name = target.name;
+  }
+  handleChange(e) {
+    let target = e.target;
+    let value = target.type === 'checkbox' ? target.checked : target.value;
+    let name = target.name;
 
-        this.setState({
-          [name]: value
-        });
-    }
+    this.setState({
+      [name]: value
+    });
+  }
 
-    handleSubmit(e) {
-        e.preventDefault();
+  handleSubmit(e) {
+    e.preventDefault();
+    let  data=JSON.stringify({
+      "email":e.target.elements.email.value,
+      "password":e.target.elements.password.value
+    })
 
-        console.log('The form was submitted with the following data:');
-        console.log(this.state);
-    }
+      console.log(data);
+    this.setState({login: !this.state.login});
+    axios.get('http://localhost:8080/homepage/signin',
 
-    render() {
-        return (
+      data,{
+      headers:{ 'Content-Type': 'application/json;charset=UTF-8'}
+
+    })
+    .then(request =>{
+      console.log('The form was submitted sucess');
+    })
+    console.log('The form was submitted with the following data:');
+  }
+
+  render() {
+    return (
 
 
-        <div className="FormCenter" >
-            <form onSubmit={this.handleSubmit} className="FormFields" onSubmit={this.handleSubmit}>
-            <div className="FormField">
-                <label className="FormField__Label" htmlFor="email">E-Mail Address</label>
-                <input type="email" id="email" className="FormField__Input" placeholder="Enter your email" name="email" value={this.state.email} onChange={this.handleChange} />
-              </div>
+      <div className="FormCenter" >
+      <form onSubmit={this.handleSubmit} className="FormFields" onSubmit={this.handleSubmit}>
+      <div className="FormField">
+      <label className="FormField__Label" htmlFor="email">E-Mail Address</label>
+      <input type="email" id="email" className="FormField__Input" placeholder="Enter your email" name="email" value={this.state.email} onChange={this.handleChange} />
+      </div>
 
-              <div className="FormField">
-                <label className="FormField__Label" htmlFor="password">Password</label>
-                <input type="password" id="password" className="FormField__Input" placeholder="Enter your password" name="password" value={this.state.password} onChange={this.handleChange} />
-              </div>
+      <div className="FormField">
+      <label className="FormField__Label" htmlFor="password">Password</label>
+      <input type="password" id="password" className="FormField__Input" placeholder="Enter your password" name="password" value={this.state.password} onChange={this.handleChange} />
+      </div>
 
-              <div className="FormField">
-                  <button className="FormField__Button mr-20">Sign In</button> <Link to="/" className="FormField__Link">Create an account</Link>
-              </div>
-            </form>
+      <div className="FormField">
+      <button className="FormField__Button mr-20">Sign In</button> <Link to="/" className="FormField__Link">Create an account</Link>
+      </div>
+      </form>
 
-          </div>
-        );
-    }
+      </div>
+    );
+  }
 }
 
 export default SignInForm;
