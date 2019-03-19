0d479da71bb003ad1760441405f23b9abf09057a
shengzhizhou <40223156+shengzhizhou@users.noreply.github.com>
2019-03-19 15:37:23 -0400

    trash_update

--- map-app-react/src/App.js
+++ map-app-react/src/App.js
@@ -18,7 +18,7 @@ class App extends Component {
         <Map/>
         </div>
           <div className="App__Form" >
-
+              {/*<Route exact path="/" component={home}></Route>*/}
             <div className="PageSwitcher">
                 <NavLink to="/sign-in" activeClassName="PageSwitcher__Item--Active" className="PageSwitcher__Item">Sign In</NavLink>
                 <NavLink exact to="/sign-up" activeClassName="PageSwitcher__Item--Active" className="PageSwitcher__Item">Sign Up</NavLink>
