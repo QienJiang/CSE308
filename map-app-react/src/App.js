c891b79f25e1314351de85342b5bc56531b98d48
QienJiang <a804242681lrm@gmail.com>
2019-03-29 17:04:35 -0400

    Revert "commit x"
    This reverts commit d5f282dea20e15e1528442b0812b1f9788edf76b.

--- map-app-react/src/App.js
+++ map-app-react/src/App.js
@@ -35,6 +35,27 @@ class App extends Component {
 
       </div>
 
+      </div>
+        <div className="App">
+        <div className="Map">
+        <Map/>
+        </div>
+          <div className="App__Form" >
+              {/*<Route exact path="/" component={home}></Route>*/}
+            <div className="PageSwitcher">
+                <NavLink to="/sign-in" activeClassName="PageSwitcher__Item--Active" className="PageSwitcher__Item">Sign In</NavLink>
+                <NavLink exact to="/sign-up" activeClassName="PageSwitcher__Item--Active" className="PageSwitcher__Item">Sign Up</NavLink>
+                <NavLink exact to="/Guest" activeClassName="PageSwitcher__Item--Active" className="PageSwitcher__Item">Guest</NavLink>
+              </div>
+
+          
+              <Route exact path="/sign-up" component={SignUp}>
+              </Route>
+              <Route path="/sign-in" component={SignIn}>
+              </Route>
+              <Route exact path="/Guest" component={Guest}>
+              </Route>
+          </div>
 
         </div>
       </Router>
