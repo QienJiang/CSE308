d5f282dea20e15e1528442b0812b1f9788edf76b
QienJiang <a804242681lrm@gmail.com>
2019-03-29 15:26:12 -0400

    commit x
    xx

--- map-app-react/src/App.js
+++ map-app-react/src/App.js
@@ -35,27 +35,6 @@ class App extends Component {
       </Route>
       </div>
 
-      </div>
-        <div className="App">
-        <div className="Map">
-        <Map/>
-        </div>
-          <div className="App__Form" >
-              {/*<Route exact path="/" component={home}></Route>*/}
-            <div className="PageSwitcher">
-                <NavLink to="/sign-in" activeClassName="PageSwitcher__Item--Active" className="PageSwitcher__Item">Sign In</NavLink>
-                <NavLink exact to="/sign-up" activeClassName="PageSwitcher__Item--Active" className="PageSwitcher__Item">Sign Up</NavLink>
-                <NavLink exact to="/Guest" activeClassName="PageSwitcher__Item--Active" className="PageSwitcher__Item">Guest</NavLink>
-              </div>
-
-          
-              <Route exact path="/sign-up" component={SignUp}>
-              </Route>
-              <Route path="/sign-in" component={SignIn}>
-              </Route>
-              <Route exact path="/Guest" component={Guest}>
-              </Route>
-          </div>
 
         </div>
       </Router>
