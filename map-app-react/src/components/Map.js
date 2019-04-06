5a37292dd744352bbbbc0779e189fb977b980a02
linzengxian <linzengxian1997@gmail.com>
2019-04-05 20:45:42 -0700

    update

--- map-app-react/src/components/Map.js
+++ map-app-react/src/components/Map.js
@@ -4,6 +4,7 @@ import "leaflet/dist/leaflet.css";
 import styled from "styled-components";
 import "leaflet-realtime";
 import "leaflet-ajax";
+import "./Map.css"
 
 import { Container, Row, Col }  from "react-bootstrap";
 
@@ -14,6 +15,21 @@ height: ${props => props.height};
 `;
 
 export default class Map extends React.Component{
+    constructor (props){
+        super (props);
+        this.state ={
+            info:'',
+            data: [],
+            open: true
+        };
+        this.stateStyle = this.stateStyle.bind(this);
+        this.precinctStyle = this.precinctStyle.bind(this);
+        this.highlightFeature = this.highlightFeature.bind(this);
+        this.resetHighlight = this.resetHighlight.bind(this);
+
+        this.zoomToFeature = this.zoomToFeature.bind(this);
+        this.onEachFeature = this.onEachFeature.bind(this);
+    }
     stateStyle(feature) {
         return {
             fillColor: 'white',
@@ -31,11 +47,36 @@ export default class Map extends React.Component{
             color: 'white',
         };
     }
+    highlightFeature(e) {
+        var layer = e.target;
+
+        layer.setStyle({
+            weight: 5,
+            color: '#666',
+            dashArray: '',
+            fillOpacity: 0.7
+        });
+
+        if (!L.Browser.ie && !L.Browser.opera && !L.Browser.edge) {
+            layer.bringToFront();
+        }
+        this.info.update(layer.feature.properties);
+    }
+    resetHighlight(e) {
+        this.stateLayer.resetStyle(e.target);
+        this.layer.resetStyle(e.target);
+        this.info.update();
+    }
+    onEachFeature(feature, layer) {
+        layer.on({
+            mouseover: this.highlightFeature,
+            mouseout: this.resetHighlight,
+            click: this.zoomToFeature
+        });
+    }
   componentDidMount(){
     this.mymap = L.map(this.refs.mymap).setView([37.8, -70], 4);
       this.popup = L.popup();
-
-    this.mymap.on('click', this.onMapClick);
     // L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
       //       attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
       //https://api.mapbox.com/styles/v1/ccall/cju4omhh623za1flgiymq3do0/tiles/256/{z}/{x}/{y}@2x?access_token=pk.eyJ1IjoiY2NhbGwiLCJhIjoiY2p1NG9qemVhMTAxazQ0cDg1NWoweW5kYSJ9.f45ljFqvaHsgWlC1VjJ-Iw
@@ -50,14 +91,14 @@ export default class Map extends React.Component{
       this.realtime = L.realtime({
           url: 'https://wanderdrone.appspot.com/',
       }, {
-          interval: 3 * 1000
+          interval: 3 * 10000
       }).addTo(this.mymap);
 
       this.realtime.on('update', () =>{
           console.log('233');
       });
-      this.stateLayer = L.geoJson.ajax("https://raw.githubusercontent.com/QienJiang/CSE308/master/map-app-react/public/nycapa.json",{style: this.stateStyle});
-       this.layer = L.geoJson.ajax("https://raw.githubusercontent.com/QienJiang/CSE308/master/map-app-react/public/ny_final.json",{style: this.precinctStyle});
+      this.stateLayer = L.geoJson.ajax("https://raw.githubusercontent.com/QienJiang/CSE308/master/map-app-react/public/nycapa.json",{style: this.stateStyle,onEachFeature: this.onEachFeature});
+      this.layer = L.geoJson.ajax("https://raw.githubusercontent.com/QienJiang/CSE308/master/map-app-react/public/ny_final.json",{style: this.precinctStyle,onEachFeature: this.onEachFeature});
       this.mymap.addLayer(this.stateLayer);
       this.mymap.on('zoomend', () =>{
           if (this.mymap.getZoom() >6){
@@ -70,7 +111,20 @@ export default class Map extends React.Component{
           }
       });
 
+      this.info = L.control({position: 'bottomleft'});
+      this.info.onAdd = function (map) {
+          this._div = L.DomUtil.create('div', 'info'); // create a div with a class "info"
+          this.update();
+          return this._div;
+      };
 
+      this.info.update = function (props) {
+          this._div.innerHTML = '<h4>Detail Information</h4>' +  (props ?
+              '<b>'+ 'Election_id' + props.ELECT_ID + '</b><br />' +'Population: '+ props.POP100
+              : 'Hover over a state');
+      };
+
+      this.info.addTo(this.mymap);
   /*  L.tileLayer('https://stamen-tiles-{s}.a.ssl.fastly.net/toner-labels/{z}/{x}/{y}{r}.{ext}', {
           attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
           subdomains: 'abcd',
