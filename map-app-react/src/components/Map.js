d35fd2cbbde6b3efb3228c63e5e13f93433ac07d
linzengxian <linzengxian1997@gmail.com>
2019-05-15 20:01:35 -0700

    fix bug

--- map-app-react/src/components/Map.js
+++ map-app-react/src/components/Map.js
@@ -120,7 +120,6 @@ export default class Map extends React.Component{
             weight: 1,
             color: 'white'
         });
-        e.target.closePopup();
         //this.paLayer.resetStyle(e.target);
     }
     onEachFeature(feature, layer) {
